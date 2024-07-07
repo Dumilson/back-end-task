241b7e0a1b48f031e32d37f546856464e39538ec
Domingos Braganha <domingosbreganha9@gmail.com>
2024-07-07 15:21:23 +0100

    fix: delete code

--- config/cors.php
+++ config/cors.php
@@ -15,23 +15,6 @@ return [
     |
     */
 
-    // 'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
-    // 'allowed_methods' => ['*'],
-
-    // 'allowed_origins' => ['*'],
-
-    // 'allowed_origins_patterns' => [],
-
-    // 'allowed_headers' => ['*'],
-
-    // 'exposed_headers' => [],
-
-    // 'max_age' => 0,
-
-    // 'supports_credentials' => false,
-
-
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
